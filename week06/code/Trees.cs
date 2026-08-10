d8a0ba626bca5c63b051a7c171269ef60b64d73e
mattonome <mattj4all@yahoo.com>
2026-08-10 06:17:51 +0100

    week 06 assignment complete

--- week06/code/Trees.cs
+++ week06/code/Trees.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
 public static class Trees
 {
     /// <summary>
@@ -48,6 +52,23 @@ public static class Trees
     /// <param name="bst">the BinarySearchTree in which to insert the values</param>
     private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst)
     {
-        // TODO Start Problem 5
+        // TODO Problem 5
+        // Base case: if first > last, there's nothing to insert
+        if (first > last)
+        {
+            return;
+        }
+
+        // Find the middle index
+        int mid = (first + last) / 2;
+
+        // Insert the middle value into the BST
+        bst.Insert(sortedNumbers[mid]);
+
+        // Recursively insert the left half
+        InsertMiddle(sortedNumbers, first, mid - 1, bst);
+
+        // Recursively insert the right half
+        InsertMiddle(sortedNumbers, mid + 1, last, bst);
     }
 }
